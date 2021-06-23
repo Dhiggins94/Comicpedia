8a8cca32c2ce907bb3d1307174953ed9d0d03d6f
divine higgins <divinehiggins@divines-MacBook-Pro.local>
2021-06-23 19:03:49 -0400

    buttons are functional in the dom

--- app.js
+++ app.js
@@ -7,26 +7,28 @@
 const domain_name = 'https://superheroapi.com/';
 const apikey = '2820930967944502';
 const urlName = 'https://superheroapi.com/api/2820930967944502'
+
 // function for button
 function buttonLog() {
   console.log(button.value)
   superHeroInfo(button)
 }
-const heroButton = document.querySelector('input')
-const villainButton = document.querySelector('input')
+const heroButton = document.querySelector('.doog')
+const villainButton = document.querySelector('.dab')
+
 
 // place where things will append to
 let container = document.querySelector(".info-placement")
 
 // getting the data from the api
-const superHeroInfo = async () => {
+const superHeroInfo = async (id) => {
   try {
-    const getApiUrl =  await axios.get(`https://intense-inlet-61203.herokuapp.com/superheroapi.com/api/2820930967944502&s=${button.value}`)
-    // const getApiUrl = ` https://intense-inlet-61203.herokuapp.com/superheroapi.com/api/2820930967944502/search/batman`
+    // const getApiUrl =  await axios.get(`https://intense-inlet-61203.herokuapp.com/superheroapi.com/api/2820930967944502&s=${button.value}`)
+    const getApiUrl = ` https://intense-inlet-61203.herokuapp.com/superheroapi.com/api/2820930967944502/${id}`
     const response = await axios.get(getApiUrl)
-    const pressHero = getApiUrl.data.Search
     console.log(response)
-    return pressHero
+    // const pressHero = getApiUrl.data.Search
+    // return pressHero
   } catch (error) {
     console.error(error)
   }
@@ -34,10 +36,28 @@ const superHeroInfo = async () => {
 }
 superHeroInfo()
 
-// iterate over the data and append heroes to the dom
-// function findHeros(hero) {
-// for (let i = 0; i < hero.length; i++) {
- 
-  
-// }
-// }
+// iterate over the data and append heroes to the dom/button.
+
+let heroId = [1,2,3,4,5,6,7,8,9,10]
+let GoodId = heroId[Math.floor(Math.random() * heroId.length -1)]
+console.log(heroId[Math.floor(Math.random() * heroId.length -1)]
+)
+
+let villainsId = [11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+let badId = (villainsId[Math.floor(Math.random() * villainsId.length - 1)])
+
+console.log(villainsId[Math.floor(Math.random() * villainsId.length - 1)])
+
+
+// iterate over the data and append villains to the dom/button
+heroButton.addEventListener("click",() => {
+  superHeroInfo(GoodId)
+})
+
+villainButton.addEventListener("click", () => {
+  superHeroInfo(badId)
+})
+
+
+// call them by biography? find way to call them by their alignment
+
