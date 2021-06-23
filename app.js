7d20188ac77cbd13d44b64de905799328a05b682
divine higgins <divinehiggins@divines-MacBook-Pro.local>
2021-06-23 16:46:25 -0400

    javascript has been edited,redone the buttons

--- app.js
+++ app.js
@@ -7,40 +7,37 @@
 const domain_name = 'https://superheroapi.com/';
 const apikey = '2820930967944502';
 const urlName = 'https://superheroapi.com/api/2820930967944502'
+// function for button
+function buttonLog() {
+  console.log(button.value)
+  superHeroInfo(button)
+}
+const heroButton = document.querySelector('input')
+const villainButton = document.querySelector('input')
 
+// place where things will append to
+let container = document.querySelector(".info-placement")
 
-
-let button1 = document.querySelector("hero")
-let button2 = document.querySelector("Villain")
-let container = document.querySelector()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// getting the data from the api
 const superHeroInfo = async () => {
   try {
-    const getApiUrl = ` https://intense-inlet-61203.herokuapp.com/superheroapi.com/api/2820930967944502/search/batman`
+    const getApiUrl =  await axios.get(`https://intense-inlet-61203.herokuapp.com/superheroapi.com/api/2820930967944502&s=${button.value}`)
+    // const getApiUrl = ` https://intense-inlet-61203.herokuapp.com/superheroapi.com/api/2820930967944502/search/batman`
     const response = await axios.get(getApiUrl)
+    const pressHero = getApiUrl.data.Search
     console.log(response)
-
+    return pressHero
   } catch (error) {
     console.error(error)
   }
 
-
 }
 superHeroInfo()
+
+// iterate over the data and append heroes to the dom
+// function findHeros(hero) {
+// for (let i = 0; i < hero.length; i++) {
+ 
+  
+// }
+// }
