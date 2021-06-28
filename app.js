4a22b04c394529765cb39f6ec1535e36684021eb
divine higgins <divinehiggins@divines-MacBook-Pro.local>
2021-06-28 11:37:13 -0400

    latest version

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
+// these arent tied to anything, serves as a reminder of what im using for my project
 const domain_name = 'https://superheroapi.com/';
 const apikey = '2820930967944502';
 const urlName = 'https://superheroapi.com/api/2820930967944502'
 
-
+// grabs the buttons from the html
 const heroButton = document.querySelector('.doog')
 const villainButton = document.querySelector('.dab')
 
-
+// created a container to later append things to the DOM
 let container = document.querySelector(".info-placement")
 
-
+// creates a function with an parameter of value, followed by all the everyones id's from 1 - 731 and searches randomly within itself and chooses 1, then a response variable was made to grab the api im using .followed by an if statement that checks the characters alignment for value(value being good or bad)  to return that data then recurses the superheroinfo function so it may start randomly selecting id's
 const superHeroInfo = async (value) => {
 
   const min = 1
@@ -35,7 +36,7 @@ const superHeroInfo = async (value) => {
 
  }
 
-
+// each button here is going to bring a good or bad person when pressed thanks to calling the superheroinfo function with an parameter of value
 heroButton.addEventListener("click", (e) => {
   superHeroInfo(e.target.value)
 
@@ -45,7 +46,7 @@ villainButton.addEventListener("click", (e) => {
   superHeroInfo(e.target.value)
 })
 
-
+// everything here is used for appending things to the DOM
 function showSomething(info) {
 
   const name = document.createElement("p")
@@ -75,7 +76,7 @@ function showSomething(info) {
 
 
 
-
+// this allows the removal of the elements of the current info shown, to make room for the new info when pressed. This was made possible by omdb homework assignment
 function removeContent(element) {
 
   while (element.lastChild) {
